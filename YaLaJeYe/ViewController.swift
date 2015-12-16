052ae9a8322a125f4d350f173d0d12bded8c865d
joseph Hajjar <zouz@josephs-imac.lan>
2015-12-16 23:30:56 +0200

    Initial Commit

---
+++ YaLaJeYe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YaLaJeYe
+//
+//  Created by Joseph  on 12/16/15.
+//  Copyright © 2015 Joseph hajjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
